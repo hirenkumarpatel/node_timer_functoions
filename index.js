26ae1a3647e5e72c9859c69693348a0b82438768
Hiren Patel <hirenkumarbpatel@gmail.com>
2019-08-25 14:51:54 -0400

    converting interval to percentage

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const waitTime=3000;
+const waitTime=5000;
 const waitInterval=500;
 let currentTime=0;
 
 //creating function that will called after every 0.5 seconds
 const incTime =()=>{
     currentTime+=waitInterval;
-    console.log(`waiting ${currentTime/1000} seconds..`);
-}
+    const percentTime=((currentTime/waitTime)*100);//converting interval to percenrage
+    process.stdout.clearLine();//clearing the terminal output lines
+    process.stdout.cursorTo(0);//set the cursor to first line
+    process.stdout.write(`waiting...${percentTime}%`);}
 
-const delayTime=()=>console.log('Delayed Process Finished.');
-const delayedFunction=setTimeout(delayTime,waitTime);
+const finishTime=()=>{
+    clearInterval(interval);
+    const percentTime=((currentTime/waitTime)*100);//converting interval to percenrage
+    process.stdout.clearLine();//clearing the terminal output lines
+    process.stdout.cursorTo(0);//set the cursor to first line
+    console.log('Process Finished.');
+}
+const finishedFunction=setTimeout(finishTime,waitTime);
 const interval=setInterval(incTime,500);
 
 
